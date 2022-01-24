1a72ef9ce65889c934643f9f5d88ab602757fdf7
Ondřej Bárta <email@ondrejbarta.xyz>
2022-01-24 21:00:17 +0100

    Update MusicPlayer.tsx

---
+++ src/contexts/MusicPlayer.tsx
@@ -0,0 +1,22 @@
+import React, { createContext, useContext } from "react";
+
+/* export const MusicPlayerContext = createContext<MusicPlayerContextProps>({
+  player: "",
+});
+
+export const useMusicPlayer = useContext(MusicPlayerContext);
+
+export const MusicPlayerProvider: React.FC<MusicPlayerProviderProps> = ({
+  children,
+  player,
+}) => {
+
+  
+
+
+  return (
+    <MusicPlayerContext.Provider value={player}>
+      {children}
+    </MusicPlayerContext.Provider>
+  );
+}; */
