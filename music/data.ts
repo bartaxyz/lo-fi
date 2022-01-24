8c98c4832061b33ab873fe86e977c6bb3becadae
Ondřej Bárta <email@ondrejbarta.xyz>
2022-01-24 20:54:02 +0100

    Create data.ts

---
+++ music/data.ts
@@ -0,0 +1,8 @@
+export const music = [
+  {
+    name: "Good Things Take Time",
+    artist: "Ondřej Bárta",
+    file: '/sound/OndrejBarta-GoodThingsTakeTime.mp3',
+    cover: '/covers/OndrejBarta-GoodThingsTakeTime.jpg',
+  },
+];
