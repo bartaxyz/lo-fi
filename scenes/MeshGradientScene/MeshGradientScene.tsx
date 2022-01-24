c514766d83484b42b5e23eb2881596591972c3f8
Ondřej Bárta <email@ondrejbarta.xyz>
2022-01-24 23:26:53 +0100

    Update MeshGradientScene.tsx

--- scenes/MeshGradientScene/MeshGradientScene.tsx
+++ scenes/MeshGradientScene/MeshGradientScene.tsx
@@ -1,32 +1,12 @@
-import React, { useRef } from "react";
-import { OrbitControls, useHelper } from "@react-three/drei";
+import React from "react";
 import { IcosahedronWireframe } from "./components/IcosahedronWireframe";
 import { GradientPlane } from "./components/GradientPlane";
-import { useControls } from "leva";
-import { PointLight, PointLightHelper } from "three";
 
 export const MeshGradientScene = () => {
-  const light = useRef<PointLight>();
-
-  const { name, lightPosition, lightIntensity, lightColor } = useControls({
-    name: "World",
-    lightPosition: [4, 4, 4],
-    lightIntensity: 1,
-    lightColor: '#fff',
-  });
-
-  useHelper(light, PointLightHelper);
-
   return (
     <React.Fragment>
-      <OrbitControls />
       <ambientLight />
-      <pointLight
-        ref={light}
-        intensity={lightIntensity}
-        position={lightPosition}
-        color={lightColor}
-      />
+      <pointLight intensity={1} position={[4, 4, 4]} color="blue" />
       <IcosahedronWireframe />
       <GradientPlane />
     </React.Fragment>
