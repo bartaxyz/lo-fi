64a5eac06c9fc218f8b72243ef1df09c5cb850e1
Ondřej Bárta <email@ondrejbarta.xyz>
2022-01-24 20:54:00 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, {}) => {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      use: {
+        loader: "raw-loader",
+      },
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
