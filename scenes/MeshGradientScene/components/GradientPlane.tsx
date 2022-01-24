1eec81ad16aaf5c198b03d5edc4bb372e99faecd
Ondřej Bárta <email@ondrejbarta.xyz>
2022-01-24 23:26:51 +0100

    Update GradientPlane.tsx

--- scenes/MeshGradientScene/components/GradientPlane.tsx
+++ scenes/MeshGradientScene/components/GradientPlane.tsx
@@ -1,6 +1,6 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import { MeshDistortMaterial } from "@react-three/drei";
-import { MeshProps, useFrame } from "@react-three/fiber";
+import { MeshProps } from "@react-three/fiber";
 import * as THREE from "three";
 
 import snoise from "../shaders/utils/snoise.glsl";
@@ -10,43 +10,23 @@ import fragmentShader from "../shaders/gradient/fragment.glsl";
 export const GradientPlane: React.FC<MeshProps> = (props) => {
   const gradientPlane = useRef<THREE.Mesh>();
 
-  useFrame((state, delta) => {
-    if (!gradientPlane.current) return;
-
-    const speed = 0.15;
-    /* gradientPlane.current!.rotation.x += delta * speed;
-    gradientPlane.current!.rotation.y += delta * speed;
-    gradientPlane.current!.rotation.z += delta * speed; */
-  });
-
   return (
     <React.Fragment>
       <mesh
         {...props}
         ref={gradientPlane}
-        position={new THREE.Vector3(0, 0, -20)}
+        position={new THREE.Vector3(0, 0, -28)}
       >
-        {/* <planeBufferGeometry args={[10, 10, 40, 40]} attach="geometry" /> */}
-
-        <boxBufferGeometry args={[20, 20, 20, 20, 20, 20]} attach="geometry" />
+        <boxBufferGeometry args={[50, 50, 50, 5, 5, 5]} attach="geometry" />
         <MeshDistortMaterial
           color="black"
           attach="material"
-          distort={0.5}
-          speed={10}
-          reflectivity={1}
-          sheen={10}
+          distort={0.25}
+          speed={0.25}
+          reflectivity={0.5}
+          sheen={200}
           roughness={1}
         />
-
-        {/* <shaderMaterial
-          args={[
-            {
-              vertexShader: snoise + vertexShader,
-              fragmentShader: snoise + fragmentShader,
-            },
-          ]}
-        /> */}
       </mesh>
     </React.Fragment>
   );
