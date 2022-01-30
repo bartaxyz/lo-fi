a2a1ed41943342ce57955be4a5e2c47ace45b82a
Ondřej Bárta <email@ondrejbarta.xyz>
2022-01-30 13:39:42 +0100

    Update Vinyl.tsx

--- scenes/PlayerScene/components/Vinyl.tsx
+++ scenes/PlayerScene/components/Vinyl.tsx
@@ -11,13 +11,18 @@ export const Vinyl = () => {
   const [texture1] = useTexture([songs[0].cover]);
   const vinylTexture = useTexture("/models/vinyl-texture.png");
   const vinylNormal = useTexture("/models/vinyl-normal.png");
+  const vinylInvertedNormal = useTexture("/models/vinyl-inverted-normal.png");
   const musicPlayer = useMusicPlayer();
   const state = useThree();
 
-  const controls = useControls({
+  const vinyl = useControls('Vinyl', {
     reflectivity: 100,
-    flatShading: true,
-    metalness: 0.75
+    metalness: 1.4,
+    roughness: 0.5,
+  })
+  const cover = useControls('Cover', {
+    reflectivity: 100,
+    metalness: 1.4
   })
 
   console.log({ musicPlayer });
@@ -29,18 +34,22 @@ export const Vinyl = () => {
   });
 
   useEffect(() => {
-    if (gltf && texture1 && controls) {
+    if (gltf && texture1 && cover && vinyl) {
       vinylTexture.anisotropy = state.gl.capabilities.getMaxAnisotropy();
       console.log(gltf.scene.children);
-      ((gltf.scene.children[1] as THREE.Mesh).material as MeshPhysicalMaterial).map = vinylTexture;
-      ((gltf.scene.children[1] as THREE.Mesh).material as MeshPhysicalMaterial).roughnessMap = vinylNormal;
-      // ((gltf.scene.children[1] as THREE.Mesh).material as MeshPhysicalMaterial).normalMap = vinylNormal;
+      ((gltf.scene.children[1] as THREE.Mesh).material as MeshPhysicalMaterial).map = texture1;
+      ((gltf.scene.children[1] as THREE.Mesh).material as MeshPhysicalMaterial).roughnessMap = vinylInvertedNormal;
+      ((gltf.scene.children[1] as THREE.Mesh).material as MeshPhysicalMaterial).metalnessMap = vinylNormal;
+      // ((gltf.scene.children[1] as THREE.Mesh).material as MeshPhysicalMaterial).normalMap = vinylInvertedNormal;
+      ((gltf.scene.children[1] as THREE.Mesh).material as MeshPhysicalMaterial).roughness = vinyl.roughness;
+      ((gltf.scene.children[1] as THREE.Mesh).material as MeshPhysicalMaterial).reflectivity = vinyl.reflectivity;
+      ((gltf.scene.children[1] as THREE.Mesh).material as MeshPhysicalMaterial).metalness = vinyl.metalness;
+
       ((gltf.scene.children[0] as THREE.Mesh).material as MeshPhysicalMaterial).map = texture1;
-      ((gltf.scene.children[0] as THREE.Mesh).material as MeshPhysicalMaterial).reflectivity = controls.reflectivity;
-      ((gltf.scene.children[0] as THREE.Mesh).material as MeshPhysicalMaterial).flatShading = controls.flatShading;
-      ((gltf.scene.children[0] as THREE.Mesh).material as MeshPhysicalMaterial).metalness = controls.metalness;
+      ((gltf.scene.children[0] as THREE.Mesh).material as MeshPhysicalMaterial).reflectivity = cover.reflectivity;
+      // ((gltf.scene.children[0] as THREE.Mesh).material as MeshPhysicalMaterial).metalness = cover.metalness;
     }
-  }, [gltf, texture1, controls]);
+  }, [gltf, texture1, cover, vinyl]);
 
   return (
     <Suspense fallback={null}>
