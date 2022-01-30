8b49367430b2551b12163f0bbae836ade928b353
Ondřej Bárta <email@ondrejbarta.xyz>
2022-01-30 13:39:44 +0100

    Update PlayerScene.tsx

--- scenes/PlayerScene/PlayerScene.tsx
+++ scenes/PlayerScene/PlayerScene.tsx
@@ -16,6 +16,13 @@ export const PlayerScene = () => {
         shadowMapHeight={800}
         shadowMapWidth={800}
       />
+
+      <pointLight
+        position={[10, 10, 5]}
+        castShadow={true}
+        shadowMapHeight={800}
+        shadowMapWidth={800}
+      />
       <Floor />
       {/* <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} /> */}
